5b65a91a094729e30c3e6f41a393179bcef07786
Divyam <divyam2203@gmail.com>
2024-06-10 11:21:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero/Hero.tsx';
+import Works from './components/Works/Works.tsx';
+import About from './components/About/About.tsx';
+
+
+
+export default function App(){
+  return(
+    <>
+      <Hero />
+      <Works />
+      <About />
+    </>
+  );
+}
